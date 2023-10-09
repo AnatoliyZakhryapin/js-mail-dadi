067cea12d364b223910a7b93a9460afe76e508ae
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-09 16:05:15 +0200

    ADD control for isUserMail

--- js/app.js
+++ js/app.js
@@ -8,20 +8,31 @@ const usersMailDateBase = [];
 for (i=0; i < 100; i++) {
     let n = i + 1;
     usersMailDateBase.push(`utente${n}@gmail.com`)
-    console.log(n, usersMailDateBase[i])
 }
 
-console.log(usersMailDateBase)
-
-// - Creare la variaviele "mailUtente"
+console.log( usersMailDateBase)
+// - Creare la variaviele "userMail"
+// +
+// - Chiedere ad uttente il suo indirizzo mail tramite prompt  e assegnare il
 
 let userMail = prompt("Qual'è il tuo indirizzo mai?")
-console.log(userMail)
 
-// - Chiedere ad uttente il suo indirizzo mail tramite prompt  e assegnare il sua valore alla variabile "mailUtente"
-// - Controllare se la variabile "mailUtente" è presente nella database tramite for
+// - Controllare se la variabile "userMail" è presente nella database tramite for
+// + 
 //     - Se il valore è presente - stampare "L'accesso consestito"
 //     ALTRIMENTI - "L'acesso vietato"
+
+let lenghtArray = usersMailDateBase.length;
+let isUserMail;
+
+for (let i = 0; i < lenghtArray; i++) {
+    if (usersMailDateBase[i] === userMail) {
+       isUserMail = true;
+    }
+}
+
+!isUserMail ? console.log("L'accesso NON è consestito") 
+    :  console.log("Benvenuto") 
 
 // - Creare la variabile con il numero random per pc "numeroPc"
 // - Creare la variavile con il numero random per utente "numeroUtente"
