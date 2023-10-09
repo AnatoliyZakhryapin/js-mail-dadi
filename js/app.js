f758cf9108682b588598d59249709782dba4777c
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-09 17:55:26 +0200

    ADD web game dadi

--- js/app.js
+++ js/app.js
@@ -36,28 +36,59 @@ for (let i = 0; i < lenghtArray; i++) {
 
 // # DADI
 
-// - Creazione delle variabili per i giocatori
-let numberPc;
-let numberUser;
 
-// - Assegnare il numero random alle variabile finche non sarrano diversi
-do {
-    numberPc = Math.floor(Math.random() * (6 - 1) ) + 1;
-    numberUser =  Math.floor(Math.random() * (6 - 1) ) + 1;
-  }
-  while (numberPc == numberUser);
 
-// - Confrontare i numeri e stabilire chi a piu punetggio
-let gamer1 = "PC";
-let gamer2 = "User";
-let vinner;
+// Creazione database per imagine
+const imageBase = [
+    "./img/dado1.png", 
+    "./img/dado2.png", 
+    "./img/dado3.png",
+    "./img/dado4.png",
+    "./img/dado5.png",
+    "./img/dado6.png",
+];
 
-if (numberPc > numberUser) {
-   vinner = gamer1;
-} else {
-    vinner = gamer2;
-}
+let player1DOMElement = document.getElementById("imagePlayer1");
+let player2DOMElement = document.getElementById("imagePlayer2");
+let btnStartDOMElement = document.getElementById("start");
+let vinnertDOMElement = document.getElementById("vinner");
 
-console.log("numberPC", numberPc)
-console.log("numberUser", numberUser)
-console.log(vinner);
+btnStartDOMElement.addEventListener("click", function (){
+   // - Creazione delle variabili per i giocatori
+    let numberPc;
+    let numberUser;
+
+    // - Assegnare il numero random alle variabile finche non sarrano diversi
+    do {
+        numberPc = Math.floor(Math.random() * (6 - 1) ) + 1;
+        numberUser =  Math.floor(Math.random() * (6 - 1) ) + 1;
+    }
+    while (numberPc == numberUser);
+
+    // - Confrontare i numeri e stabilire chi a piu punetggio
+    let gamer1 = "PC";
+    let gamer2 = "User";
+    let vinner;
+
+    if (numberPc > numberUser) {
+    vinner = gamer1;
+    vinnertDOMElement.innerHTML = vinner;    
+    } else {
+        vinner = gamer2;
+        vinnertDOMElement.innerHTML = vinner;  
+    }
+
+    console.log("numberPC", numberPc)
+    console.log("numberUser", numberUser)
+    console.log(vinner);
+    
+    let imagePlayer1 = imageBase[numberPc-1];
+    let imagePlayer2 = imageBase[numberUser-1];
+   
+    player1DOMElement.src = imagePlayer1;
+    player2DOMElement.src = imagePlayer2;
+})
+
+console.log(imagePlayer1)
+console.log(imagePlayer2)
+
