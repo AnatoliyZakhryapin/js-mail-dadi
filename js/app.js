68bdbd537bf2e9f932c1380f7ca0207f9d2008ea
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-09 15:40:38 +0200

    ADD datebase userMail

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// # js-mail-dadi
+
+// - Creare database con l'indirizzi mail registrati tramitte un array
+
+const usersMail = [];
+
+
+for (i=0; i < 100; i++) {
+    let n = i + 1;
+    usersMail.push(`utente${n}@gmail.com`)
+    console.log(n, usersMail[i])
+}
+
+console.log(usersMail)
+
+// - Creare la variaviele "mailUtente"
+// - Chiedere ad uttente il suo indirizzo mail tramite prompt  e assegnare il sua valore alla variabile "mailUtente"
+// - Controllare se la variabile "mailUtente" è presente nella database tramite for
+//     - Se il valore è presente - stampare "L'accesso consestito"
+//     ALTRIMENTI - "L'acesso vietato"
+
+// - Creare la variabile con il numero random per pc "numeroPc"
+// - Creare la variavile con il numero random per utente "numeroUtente"
+// - Confrontare i numeri e stabilire chi a piu punetggio
